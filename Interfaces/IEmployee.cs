bbe87c1e0df9b94c7b352f2c7d096f7d99819762
ssheikholeslam <librapropertymanager@gmail.com>
2022-10-03 23:22:56 -0400

    Add Employee Enhancement with CRUD Functionality

---
+++ Interfaces/IEmployee.cs
@@ -0,0 +1,19 @@
+﻿using Cardano_Catalyst.Models;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cardano_Catalyst.Interfaces
+{
+    public interface IEmployee
+    {
+        IMongoCollection<Employee> employeeCollection { get; }
+        IEnumerable<Employee> GetAllEmployees();
+        Employee GetEmployeeDetails(string Id);
+        void Create(Employee employee);
+        void Update(string Id, Employee employee);
+        void Delete(string Id);
+    }
+}
